ad3e754888fee189ee4f57d18b3e6ca2d670c469
Sorin Marta <sorinmarta@Sorins-MacBook-Pro.local>
2019-01-02 22:04:09 +0200

    Updates

---
+++ precache-manifest.f730f470aa3697be05309e3f7f61965a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e68a153454f1f4a4958929b2e84ae412",
+    "url": "/static/media/medium-avatar.e68a1534.png"
+  },
+  {
+    "revision": "11a7256ecfe95fc5b537cd1592b5ca9b",
+    "url": "/static/media/bg.11a7256e.png"
+  },
+  {
+    "revision": "97c5a8d8629aa399c3ca9aed6d7797c0",
+    "url": "/static/media/avatar-image.97c5a8d8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3037e14347df5d1d4644",
+    "url": "/static/js/main.3037e143.chunk.js"
+  },
+  {
+    "revision": "70a60513508cfe31eae4",
+    "url": "/static/js/1.70a60513.chunk.js"
+  },
+  {
+    "revision": "3037e14347df5d1d4644",
+    "url": "/static/css/main.6f399b63.chunk.css"
+  },
+  {
+    "revision": "1add0a621887e54ef7b9f96f93430121",
+    "url": "/index.html"
+  }
+];
